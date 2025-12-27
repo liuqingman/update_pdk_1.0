f256ba2eac5cdf3796a7d6da2dea4d20d04ea5f7
liuqingman <wuwenmanliuchao@outlook.com>
2025-12-27 21:53:21 +0800

    Update auto_extract_content.sh

--- auto_extract_content.sh
+++ auto_extract_content.sh
@@ -1,1 +1,7 @@
-echo "auto_extract_content.sh in"
+#!/bin/bash
+
+cd /home/xp/lc/pdk/ossutil
+./generate_bsp_txt.bash '${pdk_sdk_version}'
+echo "--------------------------"
+cat bsp_version.txt | head -5 > pdk_version.txt
+echo "--------------------------"
