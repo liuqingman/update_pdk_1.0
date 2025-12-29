83f29a9e99b6530ecc6500b99774bf281cc3c272
liuqingman <wuwenmanliuchao@outlook.com>
2025-12-29 15:18:46 +0800

    Update update_pdk_in_xpilot_misc_apps.sh

--- update_pdk_in_xpilot_misc_apps.sh
+++ update_pdk_in_xpilot_misc_apps.sh
@@ -10,7 +10,7 @@ git checkout -b update_${merge_to_branch}_${pdk_sdk_version}
 cat /data/liuc/ossutil/pdk_version.txt > /data/liuc/xpilot_misc_apps/bsp_version/xp5/xccp_bsp_version.txt
 git add .
 git commit -m "update new pdk ${pdk_sdk_version}"
-git show HEAD
+git show HEAD > /tmp/xpilot_misc_apps.txt
 git push --set-upstream origin update_${merge_to_branch}_${pdk_sdk_version} -f
 glab mr create --title "update pdk ${pdk_sdk_version}" --description "update pdk ${pdk_sdk_version}" --target-branch ${merge_to_branch} 2>&1 | grep "https" >> /tmp/update_pdk_sdk_mr.txt
 
