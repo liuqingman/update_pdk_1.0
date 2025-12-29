f9f1cec3e3c5ecd2ca74f329651fa8ef4332fc31
liuqingman <wuwenmanliuchao@outlook.com>
2025-12-29 15:11:00 +0800

    Fix error redirection in merge request creation

--- update_pdk_in_xpilot_misc_apps.sh
+++ update_pdk_in_xpilot_misc_apps.sh
@@ -12,5 +12,5 @@ git add .
 git commit -m "update new pdk ${pdk_sdk_version}"
 git show HEAD
 git push --set-upstream origin update_${merge_to_branch}_${pdk_sdk_version} -f
-glab mr create --title "update pdk ${pdk_sdk_version}" --description "update pdk ${pdk_sdk_version}" --target-branch ${merge_to_branch} | grep "https" >> /tmp/update_pdk_sdk_mr.txt
+glab mr create --title "update pdk ${pdk_sdk_version}" --description "update pdk ${pdk_sdk_version}" --target-branch ${merge_to_branch} 2>&1 | grep "https" >> /tmp/update_pdk_sdk_mr.txt
 
